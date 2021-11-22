56dac1948fe03fa8335b2b5e5106426d4d826223
Owenzbs <owen.koval@gmail.com>
2021-11-22 17:15:28 +0200

    Added search

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import eyed3
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['WHOOSH_BASE'] ='whoosh'
 database={'Owenzbs':'owenzbs','Solomia':'kuhar123','Peter':'Parker'}
 
 db=SQLAlchemy(app)
@@ -30,21 +31,15 @@ class musicstaff(db.Model):
     artist = db.Column(db.String(1000))
     album = db.Column(db.String(1000))
 
+
+
 @app.route('/')
 def index():
-    return render_template("login.html")
+    return render_template("index.html")
 
-@app.route('/form_login',methods=['POST','GET'])
+@app.route('/login',methods=['POST','GET'])
 def login():
-    name1=request.form['username']
-    pwd=request.form['password']
-    if name1 not in database:
-	    return render_template('login.html',info='Invalid User')
-    else:
-        if database[name1]!=pwd:
-            return render_template('login.html',info='Invalid Password')
-        else:
-	         return render_template('index.html', name=name1)
+	return render_template('index.html')
 
 @app.route('/profile', methods=['POST','GET'])
 def profile():
@@ -61,20 +56,56 @@ def profile():
         db.session.add(newfile)
         db.session.commit()
         
-        
-        
     return render_template("profile.html", message="Succesfully upload")
 
 @app.route('/music',methods=['GET'])
 def music():
     allsongs=musicstaff.query.order_by(musicstaff.id).all()
     songs = os.listdir('static/music')
-    return render_template("music.html", songs=songs, allsongs=allsongs)
+    list1=[]
+    list2=[]
+    list3=[]
+    list4=[]
+    list5=[]
+    for i in allsongs:
+        for el in songs:
+            if i.name in str(el):
+                list1.append(i.id)
+                list2.append(el)
+                list3.append(i.duration)
+                list4.append(i.artist)
+                list5.append(i.album)
+    return render_template("music.html", count=len(songs),   songs=songs, allsongs=allsongs, list1=list1, list2=list2, list3=list3 , list4=list4, list5=list5)
 
 @app.route('/allmusic')
 def allmusic():
     allsongs=musicstaff.query.order_by(musicstaff.id).all()
     return render_template("allmusic.html", allsongs=allsongs)
+
+@app.route('/search', methods=['POST','GET'])
+def search():
+    title=request.form.get("title")
+    list1=[]
+    list2=[]
+    list3=[]
+    list4=[]
+    list5=[]
+    allsongs=musicstaff.query.order_by(musicstaff.id).all()
+    songs = os.listdir('static/music')
+    if request.method=="POST":
+        for i in allsongs: 
+            if (title in i.name) :   
+                for el in songs:
+                    if (title in el) :
+                        list1.append(i.id)
+                        list2.append(el)
+                        list3.append(i.duration)
+                        list4.append(i.artist)
+                        list5.append(i.album)
+        
+    return render_template("allmusic.html", allsongs=allsongs, songs=songs, count=len(list1), result1=list1, result2=list2, result3=list3, result4=list4, result5=list5)
+    
+
 
 if __name__ == "__main__":
     db.create_all()
